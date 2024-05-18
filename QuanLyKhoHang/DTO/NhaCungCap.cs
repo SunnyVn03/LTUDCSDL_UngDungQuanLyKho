349c6bb64f5e81c36421c19a7e675bc2b33f7064
1tsukoyumi <94424899+1tsukoyumi@users.noreply.github.com>
2024-05-19 01:53:26 +0700

    Add files via upload

---
+++ QuanLyKhoHang/DTO/NhaCungCap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhoHang.DTO
+{
+    public class NhaCungCap
+    {
+        string maNhaCungCap, tenNhaCungCap, diaChi, email, dienThoai;
+        bool isDelete;
+        public string MaNhaCungCap { get { return maNhaCungCap; } set { maNhaCungCap = value; } }
+        public string TenNhaCungCap { get { return tenNhaCungCap; } set { tenNhaCungCap = value; } }
+        public string DiaChi { get { return diaChi; } set { diaChi = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public string DienThoai { get { return dienThoai; } set { dienThoai = value; } }
+        public bool IsDelete { get { return isDelete; } set { isDelete = value; } }
+    }
+}
