a24aaecfa085a137d1fe3a3245936cb0be77141a
1tsukoyumi <94424899+1tsukoyumi@users.noreply.github.com>
2024-05-19 01:52:20 +0700

    Add files via upload

---
+++ QuanLyKhoHang/DanhMuc/Frm_QuanLyHangHoa_Modifies.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace QuanLyKhoHang.DanhMuc
+{
+    partial class Frm_QuanLyHangHoa_Modifies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblThoat = new System.Windows.Forms.Label();
+            this.txtSoLuongTon = new System.Windows.Forms.TextBox();
+            this.txtLoaiHang = new System.Windows.Forms.TextBox();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.txtMaNhaCungCap = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTenHang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaHang = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblThoat
+            // 
+            this.lblThoat.AutoSize = true;
+            this.lblThoat.BackColor = System.Drawing.Color.Green;
+            this.lblThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThoat.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblThoat.Location = new System.Drawing.Point(631, 20);
+            this.lblThoat.Name = "lblThoat";
+            this.lblThoat.Size = new System.Drawing.Size(30, 29);
+            this.lblThoat.TabIndex = 45;
+            this.lblThoat.Text = "X";
+            this.lblThoat.Click += new System.EventHandler(this.lblThoat_Click);
+            // 
+            // txtSoLuongTon
+            // 
+            this.txtSoLuongTon.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoLuongTon.Location = new System.Drawing.Point(220, 212);
+            this.txtSoLuongTon.Name = "txtSoLuongTon";
+            this.txtSoLuongTon.Size = new System.Drawing.Size(446, 34);
+            this.txtSoLuongTon.TabIndex = 44;
+            // 
+            // txtLoaiHang
+            // 
+            this.txtLoaiHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLoaiHang.Location = new System.Drawing.Point(220, 177);
+            this.txtLoaiHang.Name = "txtLoaiHang";
+            this.txtLoaiHang.Size = new System.Drawing.Size(446, 34);
+            this.txtLoaiHang.TabIndex = 43;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnHuy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnHuy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(470, 296);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(178, 34);
+            this.btnHuy.TabIndex = 42;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = false;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCapNhat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCapNhat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCapNhat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCapNhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapNhat.Location = new System.Drawing.Point(202, 296);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(178, 34);
+            this.btnCapNhat.TabIndex = 41;
+            this.btnCapNhat.Text = "Cập nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = false;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // txtMaNhaCungCap
+            // 
+            this.txtMaNhaCungCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaNhaCungCap.Location = new System.Drawing.Point(220, 247);
+            this.txtMaNhaCungCap.Name = "txtMaNhaCungCap";
+            this.txtMaNhaCungCap.Size = new System.Drawing.Size(446, 34);
+            this.txtMaNhaCungCap.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(18, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(194, 29);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "Mã nhà cung cấp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 29);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Số lượng tồn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 29);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Loại hàng";
+            // 
+            // txtTenHang
+            // 
+            this.txtTenHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenHang.Location = new System.Drawing.Point(220, 142);
+            this.txtTenHang.Name = "txtTenHang";
+            this.txtTenHang.Size = new System.Drawing.Size(446, 34);
+            this.txtTenHang.TabIndex = 36;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Tên hàng";
+            // 
+            // txtMaHang
+            // 
+            this.txtMaHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaHang.Location = new System.Drawing.Point(220, 107);
+            this.txtMaHang.Name = "txtMaHang";
+            this.txtMaHang.ReadOnly = true;
+            this.txtMaHang.Size = new System.Drawing.Size(446, 34);
+            this.txtMaHang.TabIndex = 34;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(18, 112);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(105, 29);
+            this.lbl.TabIndex = 33;
+            this.lbl.Text = "Mã hàng";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(-1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(682, 69);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Cập nhật hàng hoá";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Frm_QuanLyHangHoa_Modifies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 350);
+            this.Controls.Add(this.lblThoat);
+            this.Controls.Add(this.txtSoLuongTon);
+            this.Controls.Add(this.txtLoaiHang);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.txtMaNhaCungCap);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTenHang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMaHang);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_QuanLyHangHoa_Modifies";
+            this.Text = "Frm_QuanLyHangHoa";
+            this.Load += new System.EventHandler(this.Frm_QuanLyHangHoa_Modifies_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblThoat;
+        private System.Windows.Forms.TextBox txtSoLuongTon;
+        private System.Windows.Forms.TextBox txtLoaiHang;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.TextBox txtMaNhaCungCap;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMaHang;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QuanLyKhoHang/DanhMuc/Frm_QuanLyHangHoa_Modifies.cs
@@ -0,0 +1,120 @@
+﻿using ProThucHienFormDangNhap;
+using QuanLyHangHoa.BusinessLayer;
+using QuanLyKhoHang.BusinessLayer;
+using QuanLyKhoHang.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhoHang.DanhMuc
+{
+    public partial class Frm_QuanLyHangHoa_Modifies : Form
+    {
+        public Frm_QuanLyHangHoa_Modifies()
+        {
+            InitializeComponent();
+        }
+        BLL_HangHoa db;
+        public HangHoa hangHoa;
+        string err = string.Empty;
+        public bool isAdd = true;
+
+        private void Frm_QuanLyHangHoa_Modifies_Load(object sender, EventArgs e)
+        {
+            db = new BLL_HangHoa(Cls_Main.path);
+            if (isAdd)
+            {
+                txtTenHang.Focus();
+            }
+            else
+            {
+                GanHangHoaVaoConTro(hangHoa);
+            }
+        }
+
+        private void GanHangHoaVaoConTro(HangHoa hangHoa)
+        {
+            txtMaHang.Text = hangHoa.MaHang;
+            txtTenHang.Text = hangHoa.TenHang;
+            txtLoaiHang.Text = hangHoa.LoaiHang;
+            txtSoLuongTon.Text = hangHoa.SoLuongTon.ToString();
+            txtMaNhaCungCap.Text = hangHoa.MaNhaCungCap.ToString();
+            txtTenHang.Focus();
+        }
+
+        private void ResetControl()
+        {
+            txtMaHang.ResetText();
+            txtTenHang.ResetText();
+            txtLoaiHang.ResetText();
+            txtSoLuongTon.Clear();
+            txtMaNhaCungCap.Clear();
+        }
+
+        private void lblThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            ResetControl();
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTenHang.Text))
+            {
+                if (!string.IsNullOrEmpty(txtLoaiHang.Text))
+                {
+                    if (!string.IsNullOrEmpty(txtSoLuongTon.Text))
+                    {
+                        if (!string.IsNullOrEmpty(txtMaNhaCungCap.Text))
+                        {
+                            hangHoa = new HangHoa()
+                            {
+                                MaHang = txtMaHang.Text,
+                                TenHang = txtTenHang.Text,
+                                LoaiHang = txtLoaiHang.Text,
+                                SoLuongTon = int.Parse(txtSoLuongTon.Text),
+                                MaNhaCungCap = int.Parse(txtMaNhaCungCap.Text),
+                            };
+
+                            if (db.CapNhatHangHoa(ref err, hangHoa) >= 1)
+                            {
+                                MessageBox.Show("cập nhật thành công");
+                                this.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show("cập nhật không thành công \n" + err);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("chưa nhập email");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("chưa nhập số điện thoại");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("chưa nhập địa chỉ");
+                }
+            }
+            else
+            {
+                MessageBox.Show("chưa nhập tên nhà cung cấp");
+            }
+        }
+    }
+}
