f17c1d69eb132eab64b34889c882f5eb7bff437a
1tsukoyumi <94424899+1tsukoyumi@users.noreply.github.com>
2024-05-19 01:51:47 +0700

    Add files via upload

--- QuanLyKhoHang/BusinessLayer/BLL_HangHoa.cs
+++ QuanLyKhoHang/BusinessLayer/BLL_HangHoa.cs
@@ -7,6 +7,8 @@ using QuanLyKhoHang.DataLayer;
 using System.Windows.Forms;
 using System.Data;
 using System.IO;
+using QuanLyKhoHang.DTO;
+using System.Data.SqlClient;
 
 namespace QuanLyHangHoa.BusinessLayer
 {
@@ -19,9 +21,42 @@ namespace QuanLyHangHoa.BusinessLayer
         }
 
         //phuong thuc lay ds hang hoa
-        public DataTable LayDSHangHoa(ref string err)
+        public DataTable LayDSHangHoa(ref string err, string maHang)
         {
-            return _db.GetDataTable(ref err, "HangHoa_Select", CommandType.StoredProcedure, null);
+            SqlParameter[] sqlParameters = new SqlParameter[]
+            {
+                new SqlParameter("@MaHang", maHang)
+            };
+            return _db.GetDataTable(ref err, "HangHoa_Select", CommandType.StoredProcedure, sqlParameters);
+        }
+
+        public int CapNhatHangHoa(ref string err, HangHoa hangHoa)
+        {
+            SqlParameter[] sqlParameters = new SqlParameter[] {
+                new SqlParameter("@MaHang",hangHoa.MaHang),
+                new SqlParameter("@TenHang",hangHoa.TenHang),
+                new SqlParameter("@LoaiHang",hangHoa.LoaiHang),
+                new SqlParameter("@SoLuongTon",hangHoa.SoLuongTon),
+                new SqlParameter("@MaNhaCungCap",hangHoa.MaNhaCungCap)
+            };
+            return _db.MyExcuteNonQuery(ref err, "HangHoa_InsertAndUpdate", CommandType.StoredProcedure, sqlParameters);
+        }
+
+        internal int XoaHangHoaTheoID(ref string err, string maHang)
+        {
+            try
+            {
+                SqlParameter[] sqlParameters = new SqlParameter[] {
+                    new SqlParameter("@MaHang", maHang)
+                };
+                _db.GetDataTable(ref err, "HangHoa_Delete", CommandType.StoredProcedure, sqlParameters);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+
         }
     }
 }
